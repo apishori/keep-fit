cdfccdf6d790163e0ab51b355ea0a4a0172eac6a
jahn123 <ahnjason@usc.edu>
2021-03-20 19:27:30 -0700

    basic functionality

--- frontend/src/Cals.js
+++ frontend/src/Cals.js
@@ -1,12 +1,160 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { createContext, useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Cals = () =>{
-	return(
-		<View style={{flex:1}}>
-			<Text>Cals Screen</Text>
+const Stack = createStackNavigator();
+
+const ExerciseList= () => {
+	const navigation = useNavigation();
+	const EXERCISEDATA = [
+		{
+			name: 'Swimming'
+		},
+		{
+			name: 'Biking'
+		},
+		{
+			name: 'Running'
+		},
+	];
+	
+	const renderItem = ({ item }) => {
+		return (
+            <>
+                <Text>{item.name}</Text>
+                <Button
+                    title='Go to counter'
+                    onPress={() => navigation.navigate('counter')} // TODO: add params later
+                />
+            </>
+		);
+	};
+
+	return (
+        <View>
+            <Text>Exercises</Text>
+            <FlatList
+                data={EXERCISEDATA}
+                renderItem={renderItem}
+            />	
+        </View>	
+	);
+};
+
+const Timer = ({ timeInHours, setTimeInHours }) => {
+	const [seconds, setSeconds] = useState(0);
+	const [minutes, setMinutes] = useState(0);
+	const [hours, setHours] = useState(0);
+	const [isTiming, setIsTiming] = useState(false);
+
+	useEffect(() => {
+		if (isTiming) {
+			const updateTimer = setInterval(() => {
+				if (seconds != 59) {
+					setSeconds(seconds + 1);
+				}
+				else {
+					setSeconds(0);
+					if (minutes != 59) {
+						setMinutes(minutes + 1);
+					}
+					else {
+						setMinutes(0);
+						if (hours != 24) {
+							setHours(hours + 1);
+						}
+						else {
+							// need to create a message about max time
+						}
+					}
+				}
+				setTimeInHours(timeInHours + 1/3600);
+			}, 1000);
+		
+			return () => clearInterval(updateTimer);
+		}
+	});
+
+	const resetTimer = () => {
+		setIsTiming(false);
+		setHours(0);
+		setMinutes(0);
+		setSeconds(0);
+	}
+
+	const timeFormat = new Intl.NumberFormat(undefined, { minimumIntegerDigits: 2 });
+	
+	return (
+		<View>
+			<Text>
+				{timeFormat.format(hours)}:{timeFormat.format(minutes)}:{timeFormat.format(seconds)}
+			</Text>
+			<Button
+				title='Start'
+				onPress={() => setIsTiming(true)}
+			/>
+			<Button
+				title='Pause'
+				onPress={() => setIsTiming(false)}
+			/>
+			<Button
+				title='Stop'
+				onPress={() => resetTimer()}
+			/>
 		</View>
-	)
-}
+	);
+};
 
-export default Cals
+const CalorieCounter = () => {
+
+	const [timeInHours, setTimeInHours] = useState(0); // dummy val
+	// TODO: get MET values & might have to use gender as well
+	const caloriesBurned = /*weight * MET **/ timeInHours;
+	
+	return (
+		<>
+			<Timer
+				timeInHours={timeInHours}
+				setTimeInHours={setTimeInHours}
+			/>
+			<Text>
+				Exercise: {}
+				Calories burnt so far: {caloriesBurned.toFixed(4)}
+				<Button
+					title='Reset calories'
+					onPress={() => setTimeInHours(0)}
+				/>
+			</Text>
+		</>
+	);
+};
+
+function Cals() {
+	const [exerciseID, setExercise] = useState(-1);
+
+	return (
+		<Stack.Navigator>
+			<Stack.Screen
+				name='exerciseList' component={ExerciseList} options={{headerShown:false}}
+			/>
+			<Stack.Screen
+				name='counter' component={CalorieCounter} options={{headerShown:false}}
+			/>
+		</Stack.Navigator>
+	);
+};
+
+const styles = StyleSheet.create({
+    exercisePage: {
+        flex: 6,
+    },
+    list: {
+        flex: 5.5,
+    },
+    entry: {
+        /*flex: ,*/
+    },
+});
+
+export default Cals;
--- frontend/src/Search.js
+++ frontend/src/Search.js
@@ -1,12 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { useState, useNavigation } from 'react';
+import { StyleSheet, Text, TextInput, Button, FlatList, View } from 'react-native';
 
-const Search = () =>{
+const SearchResults = () => {
+	//const navigation = useNavigation();
+	const SEARCHRESULTDATA = [
+		{
+			title:	'title0',
+			exercise: 'Swimming',
+			nickname: 'nickname0'
+		},
+		{
+			title: 'title1',
+			exercise: 'Biking',
+			nickname: 'nickname1'
+		},
+		{
+			title: 'title2',
+			exercise: 'Running',
+			nickname: 'nickname2'
+		},
+	];
+	
+	const renderItem = ({ item }) => {
+		return (
+            <>
+                <Text>
+					{item.title}
+					{item.nickname}
+					{item.exercise}
+				</Text>
+                <Button
+                    title='Watch video/stream'
+                    //onPress={() => navigation.goBack()} //navigate('counter')} // TODO: add params later
+                />
+            </>
+		);
+	};
+
+	return (
+        <>
+            <FlatList
+                data={SEARCHRESULTDATA}
+                renderItem={renderItem}
+            />	
+        </>	
+	);
+};
+
+const SearchStatus = ({ numOfResults, searchTerm }) => {
+	const [status, setStatus] = useState('');
+
+	useEffect(() => {
+		if (searchTerm === '') {
+			setStatus('No results');
+		}
+		else {
+			setStatus(numOfResults + ' results for "' + searchTerm + '"');
+		}			
+	});
+
+	return <Text>{status}</Text>;
+};
+
+const Search = () => {
+	const [searchTerm, setSearchTerm] = useState('');
+	const [numOfResults, setNumOfResults] = useState(0);
+	const searchCount = 0;
+	
+	function OnSubmitEdit() {
+		setNumOfResults(3);
+	};
+
 	return(
 		<View style={{flex:1}}>
-			<Text>Search Screen</Text>
+			<TextInput
+				onChangeText={searchTerm => setSearchTerm(searchTerm)}
+				onSubmitEditing={event => {nativeEvent: {searchTerm, searchCount, OnSubmitEdit}}}
+				returnKeyLabel='search'
+				placeholder='Search'
+			>
+			</TextInput>
+			<SearchStatus
+				numOfResults={numOfResults}
+				searchTerm={searchTerm}
+			/>
+			<SearchResults/>
 		</View>
-	)
-}
+	);
+};
 
-export default Search
+export default Search;
