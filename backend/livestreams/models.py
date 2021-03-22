34ff292b81232ee5de4173c596396e0a6a2009ec
Joseph Yoon <josephyy@usc.edu>
2021-03-21 21:03:45 -0700

    Livestream Models/Urls/Views

--- backend/livestreams/models.py
+++ backend/livestreams/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.utils import timezone
+from users.models import User
 
 # Create your models here.
+class Livestream(models.Model):
+
+    RUN = 'R'
+    YOGA = 'Y'
+    HOME_CARDIO = 'HC'
+    TENNIS = 'T'
+    SWIMMING = 'S'
+    BASKETBALL = 'B'
+    CYCLING = 'C'
+    JUMP_ROPE = 'J'
+    HIKING = 'H'
+    OTHER = 'O'
+
+    WORKOUT_CATEGORIES = [(RUN, 'Run'),
+                   (YOGA, 'Yoga'),
+                   (HOME_CARDIO, 'Home cardio'),
+                   (TENNIS, 'Tennis'),
+                   (SWIMMING, 'Swimming'),
+                   (BASKETBALL, 'Basketball'),
+                   (CYCLING, 'Cycling'),
+                   (JUMP_ROPE, 'Jump rope'),
+                   (HIKING, 'Hiking'),
+                   (OTHER, 'Other')]
+
+    #Using MET Values from:
+    #https://www.acefitness.org/education-and-resources/professional/expert-articles/6434/5-things-to-know-about-metabolic-equivalents/
+
+    MET_VALS = {
+            "R": 9.8,
+            "Y": 2.5,
+            "HC": 4.0,
+            "T": 8.0,
+            "S": 5.8,
+            "B": 6.5,
+            "C": 8.0,
+            "J": 12.3,
+            "H": 7.3,
+            "O": 4.0,
+    }
+
+
+    stream_id = models.IntegerField(primary_key=True, blank=False, null=False, unique=True)
+    video_APIKey = models.CharField(max_length=255, blank=False, null=False, unique=False)
+	title = models.CharField(max_length=255, blank=False, null=False, unique=False)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	# This should be username of user who starts it
+	start_time = models.DateTimeField(default=timezone.now)
+	category = category = models.CharField(max_length=2, blank=False, null=False, choices=WORKOUT_CATEGORIES, default=OTHER)
+
+
+	@property
+	def calorie_bpm_count(self):
+        #RETURNS CALORIES BURNED PER MINUTE
+        # = METs x 3.5 x (body weight in kilograms) / 200 
+        return ((MET_VALS[self.category] * 3.5 * (self.author.profile.weight/2.20462)) / 200)
+
+    @property
+    def title(self):
+    	return self.title
+    
--- backend/livestreams/views.py
+++ backend/livestreams/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+class StartStreamView(APIView): # start stream
+    pass
+
+class EndStreamView(APIView): # end stream
+    pass
+
+class JoinStreamView(APIView):  # join stream
+    pass 
+    
+class LeaveStreamView(APIView):  # leave stream
+    pass
+    
+class TitleStreamListView(APIView):  # get_livestream_by_title()
+    pass
+
+class GetStreamsListView(APIView): # get_livestreams()
+    pass
+
