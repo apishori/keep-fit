d478afdbb8715ec17468147c34fab0cfaab65269
Joseph Yoon <josephyy@usc.edu>
2021-03-23 23:17:43 -0700

    Update urls.py

--- backend/livestreams/urls.py
+++ backend/livestreams/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('start/', views.StartStreamView.as_view(), name='start-livestream'), # basically the name
-    path('<str:stream_id>/', views.EndStreamView.as_view(), name='end-livestream'), # basically the name
-    path('join/<str:stream_id>/', views.JoinStreamView.as_view(), name='join-stream'), # join stream
-    path('leave/<str:stream_id>/', views.LeaveStreamView.as_view(), name='leave-stream'), # leave stream
+    path('<int:id>/', views.EndStreamView.as_view(), name='end-livestream'), # basically the name
+    path('join/<int:id>/', views.JoinStreamView.as_view(), name='join-stream'), # join stream
+    path('leave/<int:id>/', views.LeaveStreamView.as_view(), name='leave-stream'), # leave stream
     path('search/<str:title>', views.TitleStreamListView.as_view(), name='get-stream-title'), # search stream by title
-    path('streams/', views.GetStreamsListView.as_view(), name='get-stream-feed'), # get_livestreams()
+    path('', views.GetStreamsListView.as_view(), name='get-stream-feed'), # get_livestreams()
 ]
