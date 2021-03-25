96d9807905fb7fc3576f156800e03e5d0a121ad7
jahn123 <ahnjason@usc.edu>
2021-03-24 21:24:35 -0700

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View,Button } from 'react-native';
+import { useState } from 'react';
 import HomeScreen from "./src/HomeScreen";
 import Profile from "./src/Profile";
 import Search from "./src/Search";
 import Cals from "./src/Cals";
 import UploadStreamScreen from "./src/UploadStreamScreen";
-import RecordCameraScreen from "./src/RecordCameraScreen";
 import VideoPlayer from "./src/VideoPlayer";
 import UploadVideoScreen from "./src/UploadVideoScreen";
 import {MaterialIcons} from '@expo/vector-icons'
@@ -15,8 +15,14 @@ import {Provider,useSelector} from 'react-redux'
 import {createStackNavigator} from '@react-navigation/stack'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import {createStore,combineReducers} from 'redux'
+import Routers from './src/Router'
 import {NavigationContainer,DefaultTheme,useNavigation,DarkTheme,useTheme} from '@react-navigation/native'
-
+import { render } from 'react-dom';
+window.React = React
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./src/Login";
+import Register from "./src/Registration";
+import "./src/components/index.css";
 
 const rooReducer = combineReducers({
   cardData:reducer
@@ -25,10 +31,27 @@ const rooReducer = combineReducers({
 const store = createStore(rooReducer)
 const Stack = createStackNavigator()
 const Tabs = createBottomTabNavigator()
+localStorage.setItem('loginID', -1);
 
-const RootHome = ()=>{
-  const {colors} = useTheme()
+/*const RootHome = ()=>{
+
   return(
+
+  )
+}*/
+
+        /*<Stack.Navigator>
+          <Stack.Screen name="rootHome" component={RootHome} options={{headerTitle: "KeepFit", headerShown: false}} />
+          <Stack.Screen name="search" component={Search} options={{headerTitle: "Search"}} />
+          <Stack.Screen name="videoplayer" component={VideoPlayer} options={{headerTitle: "Exercise"}} />
+          <Stack.Screen name="upload" component={UploadVideoScreen} options={{headerTitle: "Upload Video"}} />
+          <Stack.Screen name="stream" component={UploadStreamScreen} options={{headerTitle: "Start Stream"}} />
+        </Stack.Navigator>*/
+
+export function Navigation() {
+  const {colors} = useTheme();
+  return ( 
+      <NavigationContainer>
     <Tabs.Navigator
       screenOptions={({ route }) => ({
       tabBarIcon: ({ color }) => {
@@ -60,38 +83,61 @@ const RootHome = ()=>{
       <Tabs.Screen name="cals" component={Cals} />
       <Tabs.Screen name="profile" component={Profile} />
     </Tabs.Navigator>
-  )
-}
-
-
-
-export function Navigation() {
-
-  return (
- 
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="rootHome" component={RootHome} options={{headerTitle: "Back", headerShown: false}} />
-          <Stack.Screen name="search" component={Search} options={{headerTitle: "Search"}} />
-          <Stack.Screen name="videoplayer" component={VideoPlayer} options={{headerTitle: "Exercise"}} />
-          <Stack.Screen name="upload" component={UploadVideoScreen} options={{headerTitle: "Upload Video"}} />
-          <Stack.Screen name="stream" component={UploadStreamScreen} options={{headerTitle: "Start Stream"}} />
-          <Stack.Screen name="camera" component={RecordCameraScreen} options={{headerTitle: "Record Video"}} />
-        </Stack.Navigator>
       </NavigationContainer>
    
   );
 }
 
-export default function App() {
-  
+function App() {
   return (
     <Provider store={store}>
-      <Navigation />
+        <Navigation />
     </Provider>
-    
   );
 }
+/*else {
+  return (
+    <Login/>
+  );
+}*/
+
+  /*return (
+    
+  
+  <Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}></Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={Register} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/home" component={HomeScreen} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
+}*/
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
