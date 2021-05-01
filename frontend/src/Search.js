3fb6c0777c55e1710ef6b3a741a1288ba4ddd413
jahn123 <ahnjason@usc.edu>
2021-04-30 22:24:33 -0700

    added search history

--- frontend/src/Search.js
+++ frontend/src/Search.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, Button, FlatList, View, Pressable, Image, Modal } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, FlatList, View, Pressable, Image } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import VideoCard from './components/VideoCard';
@@ -13,11 +13,82 @@ import StreamPlayer from './StreamPlayer';
 
 const Stack = createStackNavigator();
 
+const streamData = [
+	{
+		"kind": "youtube#searchResult",
+		"etag": "f-DJyUbk5bdYRDfiSUbzZ_mda1k",
+		"id": {
+		  "kind": "youtube#video",
+		  "videoId": "5qap5aO4i9A"
+		},
+		"snippet": {
+		  "publishedAt": "2020-02-22T19:51:37Z",
+		  "channelId": "UCSJ4gkVC6NrvII8umztf0Ow",
+		  "title": "lofi hip hop radio - beats to relax/study to",
+		  "description": "Thank you for listening, I hope you will have a good time here :) Listen to the playlist on Spotify, Apple music and more → https://bit.ly/chilledcow-playlists ...",
+		  "thumbnails": {
+			"default": {
+			  "url": "https://i.ytimg.com/vi/5qap5aO4i9A/default_live.jpg",
+			  "width": 120,
+			  "height": 90
+			},
+			"medium": {
+			  "url": "https://i.ytimg.com/vi/5qap5aO4i9A/mqdefault_live.jpg",
+			  "width": 320,
+			  "height": 180
+			},
+			"high": {
+			  "url": "https://i.ytimg.com/vi/5qap5aO4i9A/hqdefault_live.jpg",
+			  "width": 480,
+			  "height": 360
+			}
+		  },
+		  "channelTitle": "ChilledCow",
+		  "liveBroadcastContent": "live",
+		  "publishTime": "2020-02-22T19:51:37Z"
+		}
+	  },
+	  {
+	"kind": "youtube#searchResult",
+	"etag": "H8Dl3gIIC5qiAUqxCH6m9VqrdSE",
+	"id": {
+	  "kind": "youtube#video",
+	  "videoId": "DWcJFNfaw9c"
+	},
+	"snippet": {
+	  "publishedAt": "2020-02-25T19:00:14Z",
+	  "channelId": "UCSJ4gkVC6NrvII8umztf0Ow",
+	  "title": "lofi hip hop radio - beats to sleep/chill to",
+	  "description": "Welcome to the sleepy lofi hip hop radio. This playlist contains the smoothest lofi hip hop beats, perfect to help you chill or fall asleep Listen to the playlist on ...",
+	  "thumbnails": {
+		"default": {
+		  "url": "https://i.ytimg.com/vi/DWcJFNfaw9c/default_live.jpg",
+		  "width": 120,
+		  "height": 90
+		},
+		"medium": {
+		  "url": "https://i.ytimg.com/vi/DWcJFNfaw9c/mqdefault_live.jpg",
+		  "width": 320,
+		  "height": 180
+		},
+		"high": {
+		  "url": "https://i.ytimg.com/vi/DWcJFNfaw9c/hqdefault_live.jpg",
+		  "width": 480,
+		  "height": 360
+		}
+	  },
+	  "channelTitle": "ChilledCow",
+	  "liveBroadcastContent": "live",
+	  "publishTime": "2020-02-25T19:00:14Z"
+	}
+  }
+  ];
+
 const SearchStatus = ({ status }) => {
 	return <Text>{status}</Text>;
 };
 
-const PostResult = () => { 
+const PostResult = () => {
 	const API_KEY = `AIzaSyDD-5omLZO04LGwOytAAIeRGFxa5Xqa5CE`;
 	const YOUTUBE_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCSJ4gkVC6NrvII8umztf0Ow&eventType=live&type=video&key=${API_KEY}`
 	const dispatch = useDispatch()
@@ -31,7 +102,7 @@ const PostResult = () => {
 		setLoading(true)
 		fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=ab%workout&type=video&key=${API_KEY}`)
 		.then(res=>res.json())
-		.then(data=>{
+		.then(data => {
 			setLoading(false)
 			dispatch({type:"add",payload:data.items})
 		})
@@ -146,14 +217,20 @@ const SearchMenu = () => {
 	const [oldTerm, setOldTerm] = useState('');
 	const [searchAmong, setSearchAmong] = useState('users');
 	const [oldSearchAmong, setOldSearchAmong] = useState('users');
+	const [searchCounter, setSearchCounter] = useState(0);
+	// const [result, setResult] = useState([]);
 	const [numOfResults, setNumOfResults] = useState(0);
 	const [status, setStatus] = useState('No results');
 	const [didSearch, setDidSearch] = useState(false);
+	const [searchLog, setSearchLog] = useState([]);
 
-	const dispatch = useDispatch()
+	const dispatch = useDispatch();
+	const user = useSelector(state => {
+		return state.loginData.loginID;
+	});
 	const token = useSelector(state => {
-		return state.loginToken.token
-	})
+		return state.loginToken.token;
+	});
 
 	const fetchData = () => {
 		const USER_SEARCH = `http://127.0.0.1:8000/users/search/?query=${searchTerm}`;
@@ -162,14 +239,17 @@ const SearchMenu = () => {
 
 		if (searchAmong === 'users') {
 			axios.get(USER_SEARCH, { headers: {"Authorization": `Token ${token}`}})
-			.then(data => {
-			dispatch({ type: 'clearResult' });
-			//console.log(data.data);
-			dispatch({ type: 'storeUserResult', payload: data.data });
-			setNumOfResults(data.data.length);
+			.then(result => {
+				dispatch({ type: 'clearResult' });
+				// console.log(result);
+				dispatch({ type: 'storeUserResult', payload: result.data });
+				// console.log(result.data.length)
+				// setResult(result.data);
+				setNumOfResults(result.data.length);
+				// console.log(numOfResults)
 			})
 			.catch((error) => {
-			console.error(error);
+				console.error(error);
 			});
 		}
 		else if (searchAmong === 'posts') {
@@ -198,33 +278,59 @@ const SearchMenu = () => {
 		}
 	};
 
-	const dummyData = [
-		{
-			label: "term1",
-			value: "term1"
-		},
-		{
-			label: "term2",
-			value: "term2"
-		},
-		{
-			label: "term3",
-			value: "term3"
-		},
-	];
+	const getSearchLog = () => {
+		const GET_LOG = `http://127.0.0.1:8000/users/searchterms/?query=${user}`;
+		axios.get(GET_LOG, {
+			headers: {
+				"Authorization": `Token ${token}`
+			}
+		})
+		.then(result => {
+			// console.log(result.data)
+			const newLog = [];
+			for (let i = 0; i < result.data.length; i++) {
+				const element = result.data[i];
+				// console.log(element)
+				newLog.push({
+					label: element.term,
+					value: element.term,
+				});
+			}
+			setSearchLog(newLog);
+			console.log(searchLog)
+		})
+		.catch(error => {
+			console.error(error);
+		});
+	};
+	// initial load of search history
+	useEffect(() => {
+		getSearchLog();
+	}, []);
+	// update state
+	useEffect(() => {
+		// if (didSearch) {
+		// 	getSearchLog();
+		// }
+		// return () => setDidSearch(false)
+		if (searchCounter != 0) {
+			// setStatus(result.length + ' result(s) for "' + searchTerm + '" in ' + searchAmong);
+			getSearchLog();
+		}
+	}, [searchCounter]);
 
 	useEffect(() => {
-		if (didSearch) {
-			setOldTerm(searchTerm)
-			setOldSearchAmong(searchAmong)
-			setDidSearch(false)
+		console.log('setNewResultLengths')
+		if (searchCounter != 0) {
+			setStatus(numOfResults + ' result(s) for "' + searchTerm + '" in ' + searchAmong);		
 		}
-		setStatus(numOfResults + ' result(s) for "' + oldTerm + '" in ' + oldSearchAmong)
-	});
+		console.log(numOfResults)
+	}, [numOfResults]);
 
-	function SearchFor() {
-		fetchData();
-		setDidSearch(true);
+	const search = async () => {
+		// await setSearchTerm(searchTerm)
+		await fetchData();
+		setSearchCounter(searchCounter + 1);
 	};
 
 	return (
@@ -239,20 +345,15 @@ const SearchMenu = () => {
 					style={styles.searchInput}
 				/>
 				<DropDownPicker
-					items={dummyData}
+					items={searchLog}
 					containerStyle={{height: 40}}
 					style={{backgroundColor: '#fafafa'}}
 					itemStyle={{
 						justifyContent: 'flex-start'
 					}}
-					// onChangeItem={item => setSearchAmong(item.value)}
+					// onChangeItem={item => setSearchTerm(item.value)}
 					dropDownStyle={{backgroundColor: '#fafafa'}}
 				/>
-				{/* <FlatList
-					
-					data={dummyData}
-					renderItem={renderHistory}
-				/> */}
 				<DropDownPicker
 					items={[
 						{label: 'users', value: 'users', selected: true},
@@ -267,9 +368,23 @@ const SearchMenu = () => {
 					onChangeItem={item => setSearchAmong(item.value)}
 					dropDownStyle={{backgroundColor: '#fafafa'}}
 				/>
+				<DropDownPicker
+					items={[
+						{label: 'Home Cardio', value: 'HC'},
+						{label: 'Swimming', value: 'S'},
+						{label: 'Other', value: 'O'}
+					]}
+					placeholder='Select exercise category'
+					containerStyle={{height: 40}}
+					style={{backgroundColor: '#fafafa'}}
+					itemStyle={{
+						justifyContent: 'flex-start'
+					}}
+					dropDownStyle={{backgroundColor: '#fafafa'}}
+				/>
 				<Button
 					title='Search'
-					onPress={() => SearchFor()}
+					onPress={() => search()}
 					style={styles.searchSubmitButton}
 				/>
 			</View>
