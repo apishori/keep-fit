839e6fa9054dd98bae68c04e918304962c7eccb2
jahn123 <ahnjason@usc.edu>
2021-04-30 22:26:47 -0700

    Update Cals.js
    - added making a workout log entry
    - added dropdown menu for workout log
    - removed some buttons to make adding workout log entries simpler

--- frontend/src/Cals.js
+++ frontend/src/Cals.js
@@ -3,68 +3,111 @@ import { StyleSheet, Text, View, FlatList, Pressable, Button } from 'react-nativ
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useDispatch, useSelector } from 'react-redux';
+import DropDownPicker from 'react-native-dropdown-picker';
 import axios from 'axios';
 
 const Stack = createStackNavigator();
 const exerciseData = [
 	{
 		id: 0,
-		category: 'Run',
+		label: 'Run',
+		category: 'R',
 		MET: 9.8
 	},
 	{
 		id: 1,
-		category: 'Yoga',
+		label: 'Yoga',
+		category: 'Y',
 		MET: 2.5
 	},
 	{
 		id: 2,
-		category: 'Home cardio',
+		label: 'Home cardio',
+		category: 'HC',
 		MET: 4.0
 	},
 	{
 		id: 3,
-		category: 'Tennis',
+		label: 'Tennis',
+		category: 'T',
 		MET: 8.0
 	},
 	{
 		id: 4,
-		category: 'Swimming',
+		label: 'Swimming',
+		category: 'S',
 		MET: 5.8
 	},
 	{
 		id: 5,
-		category: 'Basketball',
+		label: 'Basketball',
+		category: 'B',
 		MET: 6.5
 	},
 	{
 		id: 6,
-		category: 'Jump rope',
+		label: 'Jump rope',
+		category: 'JR',
 		MET: 8.0
 	},
 	{
 		id: 7,
-		category: 'Hiking',
+		label: 'Hiking',
+		category: 'H',
 		MET: 7.3
 	},
 	{
 		id: 8,
-		category: 'Other',
+		label: 'Other',
+		category: 'O',
 		MET: 4.0
 	}
 ];
 
-export const ExerciseList= () => {
+const ExerciseList = () => {
 	const navigation = useNavigation();
-	const dispatch = useDispatch();
+	// const dispatch = useDispatch();
 	const [exerciseID, setExerciseID] = useState(8);
-	//const exerciseID = useSelector(() => {})
+	const [workoutLog, setWorkoutLog] = useState();
 	
+	const token = useSelector(state => {
+		return state.loginToken.token;
+	});
+
+	const getWorkoutLog = () => {
+		axios.get(`http://127.0.0.1:8000/workouts/getall/`, {
+			headers: {
+				"Authorization": `Token ${token}`
+			}
+		})
+		.then(result => {
+			console.log(result.data);
+			setWorkoutLog(result.data);
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+	};
+
+	const clearWorkoutLog = () => {
+		axios.delete(`http://127.0.0.1:8000/workouts/clean/`, {
+			headers: {
+				"Authorization": `Token ${token}`
+			}
+		})
+		.then(result => {
+			// console.log(result);
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+	};
+
 	const ItemSeperator = () => (
-			<View
-				borderStyle='solid'
-				style={styles.itemSeparator}
-			/>
+		<View
+			borderStyle='solid'
+			style={styles.itemSeparator}
+		/>
 	);
 
 	const Item = ({ item }) => (
@@ -73,7 +116,7 @@ export const ExerciseList= () => {
 			onPress={() => setExerciseID(item.id)}
 		>
 			<Text>
-				{item.category}
+				{item.label}
 			</Text>
 		</Pressable>
 	);
@@ -92,6 +135,22 @@ export const ExerciseList= () => {
 
 	return (
         <>
+			<View>
+				<DropDownPicker
+					items={workoutLog}
+					containerStyle={{height: 40}}
+					style={{backgroundColor: '#fafafa'}}
+					itemStyle={{
+						justifyContent: 'flex-start'
+					}}
+					// onChangeItem={item => setSearchAmong(item.value)}
+					dropDownStyle={{backgroundColor: '#fafafa'}}
+				/>
+				<Button
+					title='Clear log'
+					onPress={() => clearWorkoutLog()}		
+				/>
+			</View>			
             <FlatList
                 data={exerciseData}
                 renderItem={renderItem}
@@ -172,11 +231,6 @@ const Timer = ({ timeInHours, setTimeInHours }) => {
 				onPress={() => setIsTiming(false)}
 				style={{marginTop:8}}
 			/>
-			<Button
-				title='Stop'
-				onPress={() => resetTimer()}
-				style={{marginTop:8}}
-			/>
 		</View>
 	);
 };
@@ -187,28 +241,38 @@ const CalorieCounter = ({ route }) => {
 	const [MET, setMET] = useState(exerciseData[index[0]].MET);
 	const [timeInHours, setTimeInHours] = useState(0); // dummy val
 	const [weightLbs, setWeightLbs] = useState(0);
+	const [caloriesBurned, setCaloriesBurned] = useState(0);
+
 	const user = useSelector(state => {
 		//console.log(state.loginData)
 		return state.loginData.loginID;
-	})
+	});
+	const token = useSelector(state => {
+		return state.loginToken.token;
+	});
 
-	const getProfileData = () => {
-		axios.get(`http://127.0.0.1:8000/users/${user}`)
-		.then(data => {
-			console.log(data);
+	const makeWorkoutLog = () => {
+		const CREATE_LOG = `http://127.0.0.1:8000/workouts/create/`
+		const newLog = {
+			calories_burnt: caloriesBurned,
+			category: exerciseData[index[0]].category
+		}
+		axios.post(CREATE_LOG, newLog, { 
+			headers: {
+				"Authorization": `Token ${token}`
+			}
 		})
-		.catch((error) => {
+		.then(result => {
+			console.log(result)
+		})
+		.catch(error => {
 			console.error(error);
 		});
 	};
 
 	useEffect(() => {
-		if (weightLbs == 0) {
-			//getProfileData()
-		}
-	})
-
-	const [caloriesBurned, setCaloriesBurned] = useState(MET * timeInHours);
+		setCaloriesBurned(MET * timeInHours);
+	});
 
 	return (
 		<View
@@ -218,28 +282,22 @@ const CalorieCounter = ({ route }) => {
 				timeInHours={timeInHours}
 				setTimeInHours={setTimeInHours}
 			/>
-				<Text>
-				Exercise: {exerciseData[index[0]].category}
-				<Button
-					title='Back to exercise list'
-					onPress={() => navigation.navigate('exerciseList')}
-					type="clear"
-				/>
-				</Text>
+			<Text>
+				Exercise: {exerciseData[index[0]].label}
+			</Text>
 			<Text>
 				Calories burnt so far: {caloriesBurned.toFixed(4)}
-				<Button
-					title='Reset calories'
-					onPress={() => setTimeInHours(0)}
-					type="clear"
-				/>
 			</Text>
-		
+			<Button
+				title='Log workout'
+				onPress={() => makeWorkoutLog()}
+				type="clear"
+			/>
 		</View>
 	);
 };
 
-function Cals() {
+const Cals = () => {
 	return (
 		<Stack.Navigator>
 			<Stack.Screen
