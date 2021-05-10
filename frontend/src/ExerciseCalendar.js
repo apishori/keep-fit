1023b2e322c4e5a8d3884594b29758cacb97428d
Samantha Tripp <stripp@usc.edu>
2021-05-10 07:25:08 -0700

    adding agenda to calendar

--- frontend/src/ExerciseCalendar.js
+++ frontend/src/ExerciseCalendar.js
@@ -1,24 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import React, {Component} from 'react';
+import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Agenda} from 'react-native-calendars';
 
-const ExerciseCalendar = () => {
+
+export default class AgendaScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: {}
+    };
+  }
+
+  render() {
     return (
-        <View
-            style={ styles.calendarView }
-        >
-            <Calendar
-        
-            />
-        </View>
+    <View style={{flex:1,marginTop:48}}>
+    <Text style = {styles.sectionTitle}>Workout Calendar</Text>
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2021-05-16'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+      />
+    </View>
     );
-};
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Exercise: ' + strTime + ' Yoga',
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {
+        newItems[key] = this.state.items[key];
+      });
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+  }
+
+  renderItem(item) {
+    return (
+      <TouchableOpacity
+        style={[styles.item, {height: item.height}]}
+        onPress={() => Alert.alert(item.name)}
+      >
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+}
 
 const styles = StyleSheet.create({
-    calendarView: {
-        flex: 1,
-        paddingTop: '10%'
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30
+  },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 16,
+        marginBottom: 16,
+        marginLeft:16
     },
 });
-
-export default ExerciseCalendar;
